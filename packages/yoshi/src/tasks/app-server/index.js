87adabd5ce84fc8c70d043534b1b84806c79b726
Netanel Gilad <netanelg@wix.com>
2018-09-13 16:08:45 +0300

    wait for app-server port before finishing app-server task (#538)
    <!---
    Thanks for submitting a pull request 😄 !
    -->
    
    <!--- If you're changing public APIs make sure to document them (README, FAQ) -->
    
    <!--- Be as descriptive as possible when explaining what was changed. Link to an issue if one exists -->
    ### 🔦 Summary
    Fixes #523. As discussed, this may be a breaking change for projects that ignore the PORT env variable, because we will wait forever on a port that is not going to be used. (Did any one say framework? 😜 ).
    
    <!--- Describe how the proposed changes are tested -->
    ### 🗺️ Test Plan
    Added a new test to the start command that makes sure that the message is printed only when the port is available.

--- packages/yoshi/src/tasks/app-server/index.js
+++ packages/yoshi/src/tasks/app-server/index.js
@@ -5,6 +5,7 @@ const mkdirp = require('mkdirp');
 const spawn = require('cross-spawn');
 const detect = require('detect-port');
 const debounce = require('lodash/debounce');
+const waitPort = require('wait-port');
 
 let server;
 let port;
@@ -35,7 +36,7 @@ function initializeServerStartDelegate({
   debugBrkPort,
   log,
 }) {
-  return port => {
+  return async port => {
     const defaultEnv = {
       NODE_ENV: 'development',
       DEBUG: 'wix:*,wnp:*',
@@ -52,27 +53,6 @@ function initializeServerStartDelegate({
         ),
       );
     }
-
-    console.log('');
-    console.log(
-      'Application is now available at ',
-      chalk.magenta(`http://localhost:${env.PORT}${env.MOUNT_POINT || '/'}`),
-    );
-    if (debugBrkPort !== undefined) {
-      console.log(
-        'Debugger is available at ',
-        chalk.magenta(`${serverDebugHost}:${debugBrkPort}`),
-      );
-    } else if (debugPort !== undefined) {
-      console.log(
-        'Debugger is available at ',
-        chalk.magenta(`${serverDebugHost}:${debugPort}`),
-      );
-    }
-    console.log(
-      'Server log is written to ',
-      chalk.magenta('./target/server.log'),
-    );
 
     mkdirp.sync(path.resolve('target'));
     const runScripts = [serverScript];
@@ -97,6 +77,43 @@ function initializeServerStartDelegate({
         displayErrors();
       }
     });
+
+    const waitingLogTimeout = setTimeout(() => {
+      console.log('');
+      console.log(
+        `Still waiting for app-server to start (make sure it is listening on ${chalk.magenta(
+          'process.env.PORT',
+        )}...`,
+      );
+    }, 3000);
+
+    await waitPort({
+      host: 'localhost',
+      port: env.PORT,
+      output: 'silent',
+    });
+
+    clearTimeout(waitingLogTimeout);
+
+    console.log(
+      'Application is now available at ',
+      chalk.magenta(`http://localhost:${env.PORT}${env.MOUNT_POINT || '/'}`),
+    );
+    if (debugBrkPort !== undefined) {
+      console.log(
+        'Debugger is available at ',
+        chalk.magenta(`${serverDebugHost}:${debugBrkPort}`),
+      );
+    } else if (debugPort !== undefined) {
+      console.log(
+        'Debugger is available at ',
+        chalk.magenta(`${serverDebugHost}:${debugPort}`),
+      );
+    }
+    console.log(
+      'Server log is written to ',
+      chalk.magenta('./target/server.log'),
+    );
   };
 }
 
