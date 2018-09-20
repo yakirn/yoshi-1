2dbaa37e985ba2df638f1488bfb97714345fa34d
Ronen Amiel <ronen.amiel@gmail.com>
2018-09-20 16:31:58 +0300

    add support for storybook

--- packages/yoshi/config/webpack.config.storybook.js
+++ packages/yoshi/config/webpack.config.storybook.js
@@ -1,11 +1,11 @@
 const union = require('lodash/union');
 const StylableWebpackPlugin = require('stylable-webpack-plugin');
-const webpackCommonConfig = require('./webpack.config.common');
-const projectConfig = require('yoshi-config');
+const project = require('yoshi-config');
+const { localIdentName } = require('../src/constants');
+const { createCommonWebpackConfig } = require('../src/commands/webpack.config');
 
 module.exports = config => {
-  const projectName = projectConfig.name;
-  const cssModules = projectConfig.cssModules;
+  const webpackCommonConfig = createCommonWebpackConfig({ isDebug: true });
 
   config.resolve.extensions = union(
     config.resolve.extensions,
@@ -14,14 +14,79 @@ module.exports = config => {
 
   config.module.rules = [
     ...webpackCommonConfig.module.rules,
-    ...require('../src/loaders/sass')({
-      separateCss: false,
-      cssModules,
-      tpaStyle: false,
-      projectName,
-      hmr: false,
-      min: false,
-    }).client,
+
+    // Rules for Style Sheets
+    {
+      test: /\.(css|less|scss|sass)$/,
+      exclude: /\.st\.css$/,
+      rules: [
+        {
+          loader: require.resolve('style-loader'),
+          options: {
+            // Reuses a single `<style></style>` element
+            singleton: true,
+          },
+        },
+
+        {
+          oneOf: [
+            // Files ending with `.global.(css|sass|scss|less)` will be transpiled with
+            // `modules: false`
+            {
+              test: /\.global\.[A-z]*$/,
+              loader: require.resolve('css-loader'),
+              options: {
+                // https://github.com/webpack/css-loader
+                importLoaders: 1,
+                // https://github.com/css-modules/css-modules
+                modules: false,
+              },
+            },
+            {
+              // https://github.com/webpack/css-loader
+              loader: require.resolve('css-loader'),
+              options: {
+                camelCase: true,
+                localIdentName: localIdentName.long,
+                // Make sure every package has unique class names
+                hashPrefix: project.name,
+                // https://github.com/css-modules/css-modules
+                modules: project.cssModules,
+                // PostCSS, sass-loader and resolve-url-loader, so composition
+                // will work with import
+                importLoaders: 3,
+              },
+            },
+          ],
+        },
+
+        {
+          loader: require.resolve('postcss-loader'),
+          options: {
+            // https://github.com/facebookincubator/create-react-app/issues/2677
+            ident: 'postcss',
+            plugins: [require('autoprefixer')],
+            sourceMap: true,
+          },
+        },
+
+        // https://github.com/bholloway/resolve-url-loader
+        {
+          loader: require.resolve('resolve-url-loader'),
+          options: { attempts: 1 },
+        },
+
+        // https://github.com/webpack-contrib/sass-loader
+        {
+          test: /\.(scss|sass)$/,
+          loader: require.resolve('sass-loader'),
+          options: {
+            sourceMap: true,
+            includePaths: ['node_modules', 'node_modules/compass-mixins/lib'],
+          },
+        },
+      ],
+    },
   ];
 
   config.plugins = [...(config.plugins || []), new StylableWebpackPlugin()];
